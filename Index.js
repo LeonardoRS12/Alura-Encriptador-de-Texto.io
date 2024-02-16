7ad2bbd28d784a5e015ef4e1654a2b760716e6b1
LeonardoRS12 <157629044+LeonardoRS12@users.noreply.github.com>
2024-02-16 11:02:02 -0600

    Se agregaron los archivos principales
    Se agregaron los archivos principales como el html,js,css.

---
+++ Index.js
@@ -0,0 +1,68 @@
+function encriptarTexto () {
+   let textoNormal = document.getElementById("texto").value;
+   let mensaje = document.getElementById("mensaje");
+   let parrafo = document.getElementById("parrafo");
+   let candado = document.getElementById("img-candado");
+
+   let textoCifrado = textoNormal
+   .replace(/e/gi, "enter")
+   .replace(/i/gi, "imes")
+   .replace(/a/gi, "ai")
+   .replace(/o/gi, "ober")
+   .replace(/u/gi, "ufat");
+
+   if (textoNormal.lenght != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      mensaje.textContent = "Texto Encriptado con exito";
+      parrafo.textContent = "";
+      candado.src = "./Imagenes y Videos/Encriptado.png";
+   } 
+   else {
+      candado.src = "./Imagenes y Videos/Candado.png";
+      mensaje.textContent = "Ningun mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Oooops!", "Debes Ingresar algun texto", "warning")
+   }
+
+}
+
+function desencriptarTexto () {
+   let textoNormal = document.getElementById("texto").value;
+   let mensaje = document.getElementById("mensaje");
+   let parrafo = document.getElementById("parrafo");
+   let candado = document.getElementById("img-candado");
+
+   let textoCifrado = textoNormal
+   .replace(/enter/gi, "e")
+   .replace(/imes/gi, "i")
+   .replace(/ai/gi, "a")
+   .replace(/ober/gi, "o")
+   .replace(/ufat/gi, "u");
+
+   if (textoNormal.lenght != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      mensaje.textContent = "Texto Desencriptado con exito";
+      parrafo.textContent = "";
+      candado.src = "./Imagenes y Videos/Desencriptado.png";
+   } 
+   else {
+      candado.src = "./Imagenes y Videos/Candado.png";
+      mensaje.textContent = "Ningun mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Oooops!", "Debes Ingresar algun texto", "warning")
+   }
+
+   function botonEncriptar () {
+
+   var encriptar = document.querySelector("#btnencriptar");
+   encriptar.onclick = encriptarTexto;
+}
+
+function botonDesencriptar () {
+
+   var encriptar = document.querySelector("#btndesencriptar");
+   desencriptar.onclick = desencriptarTexto;
+}
+
+
+}
